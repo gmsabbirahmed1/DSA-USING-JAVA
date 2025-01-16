f0b8707afc3dad32eee44f74a12a552886026322
gmsabbirahmed1 <148498744+gmsabbirahmed1@users.noreply.github.com>
2025-01-17 00:26:46 +0600

    Create FindAVG.java

---
+++ FindAVG.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void findAvg(int a , int b,int c){
+       int sum=a+b+c;
+       int avg=sum/3;
+        System.out.println(avg);
+    }
+    public static void main(String[] args) {
+
+        findAvg(2,3,8);
+    }
+}
